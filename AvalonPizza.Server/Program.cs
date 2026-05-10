a1370b4e9279a34e2dab5aebbd989f08fd80d697
shap234 <sha.par23@yahoo.ca>
2026-05-10 02:27:59 -0400

    create sql scripts

--- AvalonPizza.Server/Program.cs
+++ AvalonPizza.Server/Program.cs
@@ -5,15 +5,26 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        string connectionString = "Server=(localdb)\\mssqllocaldb;Database=PizzaStoreDb;Trusted_Connection=True;TrustServerCertificate=True;";
+        builder.Services.AddSingleton(connectionString);
+
         // Add services to the container.
         builder.Services.AddControllers();
 
+
+
+
+
         var app = builder.Build();
+
+        // This runs every time you hit 'Start' in Visual Studio
+        DbInitializer.Initialize(connectionString);
 
         app.MapGet("/", () => new { message = "AVALON PIZZA API" });
 
         // Configure the HTTP request pipeline.
-        app.MapControllers(); // This maps the attribute routes like [Route("api/[controller]")]
+        app.MapControllers();
 
         app.Run();
     }
---
+++ AvalonPizza.Server/Scripts/InitializeDb.sql
@@ -0,0 +1,46 @@
+﻿-- In the professional world, we call this SQL Scripting or Database Schema Scripting
+-- Create the Database
+USE [master];
+GO
+
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = N'PizzaStoreDb')
+BEGIN
+    CREATE DATABASE [PizzaStoreDb];
+END
+GO
+
+USE [PizzaStoreDb];
+GO
+
+-- Create the Table
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Pizzas]') AND type in (N'U'))
+BEGIN
+    CREATE TABLE [dbo].[Pizzas] (
+        [Id]       INT             IDENTITY (1, 1) NOT NULL, -- auto-incrementing column. It starts at 1 and increases by 1 for every new row
+        [Size]     NVARCHAR (50)   NOT NULL,
+        [Toppings] NVARCHAR (MAX)  NOT NULL,
+        [Price]    DECIMAL (18, 2) NOT NULL, -- This is standard for currency ($19.99)
+        PRIMARY KEY CLUSTERED ([Id] ASC)
+    );
+END
+GO
+
+-- Create the 'AddPizza' Stored Procedure
+CREATE OR ALTER PROCEDURE [dbo].[AddPizza]
+    @Size NVARCHAR(50),
+    @Toppings NVARCHAR(MAX),
+    @Price DECIMAL(18,2)
+AS
+BEGIN
+    INSERT INTO Pizzas (Size, Toppings, Price)
+    VALUES (@Size, @Toppings, @Price);
+END
+GO
+
+-- Create the 'GetPizzas' Stored Procedure
+CREATE OR ALTER PROCEDURE [dbo].[GetPizzas]
+AS
+BEGIN
+    SELECT Id, Size, Toppings, Price FROM Pizzas;
+END
+GO
