04e96d95e061df7f5525ecefc5915ff038cfb4cf
shap234 <sha.par23@yahoo.ca>
2026-05-12 02:08:07 -0400

    finish pizza topping respository

---
+++ AvalonPizza.Server/Interfaces/IToppingRepository.cs
@@ -0,0 +1,8 @@
+﻿using AvalonPizza.Server.Models;
+
+namespace AvalonPizza.Server.Interfaces;
+
+public interface IToppingRepository
+{
+    Task<IEnumerable<Topping>> GetAllToppingsAsync();
+}
--- AvalonPizza.Server/Repositories/ToppingRepository.cs
+++ AvalonPizza.Server/Repositories/ToppingRepository.cs
@@ -1,15 +1,23 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using AvalonPizza.Server.Interfaces;
+using AvalonPizza.Server.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
 using System.Data;
 
 namespace AvalonPizza.Server.Repositories;
 
-public class ToppingRepository
+public class ToppingRepository : IToppingRepository
 {
+    private readonly string _connectionString;
 
+    public ToppingRepository(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
     public async Task<IEnumerable<Topping>> GetAllToppingsAsync()
     {
         using var connection = new SqlConnection(_connectionString);
-
-        return await connection.QueryAsync<Topping>("usp_Toppings_GetAll", commandType: CommandType.StoredProcedure);
+        return await connection.QueryAsync<Topping>(
+            "usp_Toppings_GetAll", commandType: CommandType.StoredProcedure);
     }
 }
